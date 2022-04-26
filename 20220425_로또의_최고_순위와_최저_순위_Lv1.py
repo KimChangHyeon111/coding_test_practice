97ccce74fbeb5517378ef5505a1db26f4cfabcde
KimChangHyeon111 <56110860+KimChangHyeon111@users.noreply.github.com>
2022-04-26 23:34:05 +0900

    Day1

--- 20220425_로또의_최고_순위와_최저_순위_Lv1.py
+++ 20220425_로또의_최고_순위와_최저_순위_Lv1.py
@@ -1,3 +1,4 @@
+# My Ans
 def solution(lottos, win_nums):
     # 0의 개수. 전부 1이거나 전부 0이거나.
     poss_num = 0
@@ -19,3 +20,28 @@ def solution(lottos, win_nums):
         
     answer = [result_max, result_min]
     return answer
+
+# Top1 Ans
+def solution(lottos, win_nums):
+
+    rank=[6,6,5,4,3,2,1]
+
+    cnt_0 = lottos.count(0)
+    ans = 0
+    for x in win_nums:
+        if x in lottos:
+            ans += 1
+    return rank[cnt_0 + ans],rank[ans]
+
+# Top2 Ans
+def solution(lottos, win_nums):
+    rank = {
+        0: 6,
+        1: 6,
+        2: 5,
+        3: 4,
+        4: 3,
+        5: 2,
+        6: 1
+    }
+    return [rank[len(set(lottos) & set(win_nums)) + lottos.count(0)], rank[len(set(lottos) & set(win_nums))]]
